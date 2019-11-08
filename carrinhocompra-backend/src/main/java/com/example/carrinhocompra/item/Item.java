f743c6e63290b03568476615d6932f099f58a13d
RoDrIgO Queiroz <queiroz.rodrigo.costa@gmail.com>
2019-11-07 22:00:33 -0300

    .

---
+++ carrinhocompra-backend/src/main/java/com/example/carrinhocompra/item/Item.java
@@ -0,0 +1,67 @@
+package com.example.carrinhocompra.item;
+
+import org.springframework.data.annotation.Id;
+
+import com.example.carrinhocompra.usuario.Usuario;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+
+@AllArgsConstructor @ToString @EqualsAndHashCode
+public class Item {
+
+	@Id
+	private String id;
+
+	private String nome;
+
+	private Double valor;
+	
+    private Usuario usuario;
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Item(String id, String nome, Double valor, Usuario usuario) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.valor = valor;
+		this.usuario = usuario;
+	}
+	
+	
+
+}
